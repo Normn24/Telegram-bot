8d0906ab775cbd5dabec7933fe306b4e5fe57beb
Normn24 <den204d@gmail.com>
2023-05-25 15:06:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Cart from './Components/Cart/Cart';
+import Card from './Components/Card/Card';
+const { getData } = require("./db/db")
+const foods = getData();
 
 function App() {
+  const [cartItems, setCartItems] = useState([])
+
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
+    }
+  };
+
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id))
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        ))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className='heading'>Order foods</h1>
+      <Cart cartItems={cartItems} />
+      <div className='cards__container'>
+        {foods.map(food => {
+          return <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />;
+        })}
+      </div>
+    </>
+  )
 }
 
 export default App;
+
