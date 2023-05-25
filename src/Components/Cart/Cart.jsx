f77e65d301dc7f4e1356c75169837a480c6ed1b7
Normn24 <den204d@gmail.com>
2023-05-25 17:01:16 +0300

    f

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./Cart.css";
 import Button from "../Button/Button";
 function Cart({ cartItems, onCheckout }) {
-  const totalPrice = 0;
-  exports.totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
+  const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
 
   return (
     <div className="cart__container">
