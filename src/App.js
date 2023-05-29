00569cd35aafc6a23ba1bbf562c71c2e6da768c4
Normn24 <den204d@gmail.com>
2023-05-29 15:45:27 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Cart from './Components/Cart/Cart';
 import Card from './Components/Card/Card';
@@ -52,56 +52,36 @@ function App() {
 
   return (
     <Router>
-      <Route exact path="/">
-        <HomePage cartItems={cartItems} />
-      </Route>
-      <Route path="/order">
-        <OrderPage cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />
-      </Route>
+      <Routes>
+        <Route path="/" element={<HomePage cartItems={cartItems} />} />
+        <Route
+          path="/order"
+          element={<OrderPage cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />}
+        />
+      </Routes>
     </Router>
   );
 }
 
-function HomePage({ cartItems }) {
-  const [setCartItems] = useState([]);
-
-  const onAdd = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist) {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-        )
-      );
-    } else {
-      setCartItems([...cartItems, { ...food, quantity: 1 }]);
-    }
-  };
-
-  const onRemove = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist.quantity === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== food.id));
-    } else {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-        )
-      );
-    }
-  };
+function HomePage({ cartItems, onAdd, onRemove }) {
   return (
     <>
       <h1 className='heading'>Order foods</h1>
       <Cart cartItems={cartItems} />
       <div className='cards__container'>
         {foods.map((food) => (
-          <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          <Card
+            food={food}
+            key={food.id}
+            onAdd={onAdd} // Pass onAdd as a prop
+            onRemove={onRemove} // Pass onRemove as a prop
+          />
         ))}
       </div>
     </>
   );
 }
+
 
 function OrderPage({ cartItems, onAdd, onRemove }) {
   const totalPrice = cartItems.reduce(
